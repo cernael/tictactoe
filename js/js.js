4ad4b6ca373f8786cdca7e948458faf2a8496e52
cernael <tomas@cernael.com>
2014-02-08 04:28:40 +0100

    Some sort of actual progress

--- js/js.js
+++ js/js.js
@@ -1,19 +1,35 @@
 $(function(){
 	var Game = Object.createClass({
 		_class: "Game",
+		currentPlayer: null,
+
 		init: function(){
 			// Create board outline and game-type change buttons
 			$('<div class="controls"/>').
 				append('<div class="logo"/>').
-				append('<button type="button" id="pvp">Normal 2-player game</button>').
-				append('<button type="button" id="pve" disabled>Normal 1-player game</button>').
-				append('<button type="button" id="upvp" disabled>Ultimate 2-player game</button>').
+				append('<button type="button" id="pvp">Normal game</button>').
+				append('<button type="button" id="pve" disabled>AI game</button>').
+				append('<button type="button" id="upvp" disabled>Ultimate game</button>').
 				appendTo('body');
 
-			//instigate click handlers to create relevant Board objects
-			$('pvp').click();
-			$('pve').click();
-			$('upvp').click();
+			$('<div class="main"/>').
+				append('<div id="game"/>').
+				appendTo('body');
+
+
+			// Instigate click handlers to create relevant Board objects
+			$('#pvp').click(function(){
+				// Skapa board(depth 0), skapa två spelare, starta spelet
+				$('#game').html('');
+			});
+			$('#pve').click(function(){
+				// Skapa board(depth 0), skapa spelare + AI, starta spelet
+				$('#game').html('');
+			});
+			$('#upvp').click(function(){
+				// Skapa board(depth 1), skapa två spelare, starta spelet
+				$('#game').html('');
+			});
 		}
 
 	});
@@ -23,6 +39,30 @@ $(function(){
 		init: function (depth) {
 			this.depth = depth;
 		}
+	});
+
+	var Player = Object.createClass({
+		_class: "Player",
+		init: function(colour){
+			this.colour = colour;
+		}
+	});
+
+	var AI = Object.createClass({
+		_class: "AI",
+		_extends: Player,
+		init: function() {
+			// body...
+		}
+	});
+
+	var Marker = Object.createClass({
+		_class: "Marker",
+		owner: null,
+		init: function() {
+			// body...
+		}
 	})
-	new Game;
+
+	var game = new Game;
 });
